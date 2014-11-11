a6a5b78c718b3a7434114d88d18c000cd9db8371
Asrul Hanafi <hanafi.asrul@gmail.com>
2014-11-11 19:23:16 +0700

    info user & pass login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -25,7 +25,7 @@
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
               <div class="text-center">
-                user : admin, pass : admin
+                user : admin@admin.com, pass : password
               </div>
                 <div class="login-panel panel panel-default">
                     <div class="panel-heading">
