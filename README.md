92da01d4cf84dffe1bdddbb3a6aefcd5fc25e41a
Asrul Hanafi <hanafi.asrul@gmail.com>
2015-05-24 22:38:06 +0700

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 myIgniter
 =========
-myIgniter is a custom PHP framework for web developer. Combined between Codeigniter 3.0.0 with AdminLTE, Bootstrap and fontawesome User Interface, include auto PHP GroceryCRUD for create table with Create-Read-Update-Delete and Ion authentication system.
+myIgniter is a custom PHP framework for web developer. Combined between Codeigniter 3 with AdminLTE, and Bootstrap, include auto PHP GroceryCRUD for create table with Create-Read-Update-Delete and Ion authentication system.
 <br><br>
 <img src="http://i59.tinypic.com/2cgc7zo.png" border="0" alt="myIgniter 3.1">
--- application/views/grocery.php
+++ application/views/grocery.php
@@ -1,12 +1,12 @@
 <div class="box box-info">
 	<div class="box-header with-border">
-		<h3 class="box-title">MyIgniter</h3>
+		<h3 class="box-title">myIgniter</h3>
 		<div class="box-tools pull-right">
 		    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 		</div><!-- /.box-tools -->
 	</div><!-- /.box-header -->
 	<div class="box-body">
-		<p>myIgniter is a custom PHP framework for web developer. Combined between Codeigniter 3.0.0 with AdminLTE, Bootstrap and fontawesome User Interface, include auto PHP GroceryCRUD for create table with Create-Read-Update-Delete and Ion authentication system. </p>
+		<p>myIgniter is a custom PHP framework for web developer. Combined between Codeigniter 3 with AdminLTE, and Bootstrap, include auto PHP GroceryCRUD for create table with Create-Read-Update-Delete and Ion authentication system.</p>
 		<p>
 			<a href="https://github.com/asrul10/myIgniter/releases" class="btn btn-primary btn-lg"><i class="fa fa-download"></i> Download myIgniter</a>&nbsp;
 			<a href="https://github.com/asrul10/myIgniter" class="btn btn-info btn-lg"><i class="fa fa-github"></i> View project on GitHub</a>
